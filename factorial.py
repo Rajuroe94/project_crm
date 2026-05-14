bdc8fb427c3ede08a4c6bea021345ae9cca1919f
Raju <rajubala1994@gmail.com>
2026-05-14 10:51:04 +0530

    factorial - multiplication code

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    result = 1
+    for i in range(1,n+1):
+        result *= i
+    return result
+
+final  = factorial (5);
+print(final)
+# 1*1 = 1
+# 1*2 = 2
+# 2*3 = 6     
+# 6*4 = 24
+# 24*5 = 120  
+
+#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
---
+++ multiplication.py
@@ -0,0 +1,10 @@
+def multiplication(t,n):
+    for i in range(1,n+1):
+        print(t, "x", i, "=", t*i)
+
+multiplication(11,20)
+# 2 x 1 = 2
+# 2 x 2 = 4 
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
